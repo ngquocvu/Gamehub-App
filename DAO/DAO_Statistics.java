90feca9374c55c43a7496cde2d189385e53ed0e9
Phan Dung Tri <phandungtri4work@gmail.com>
2019-04-22 17:39:20 +0700

    DAO_Statistics (initialize)

---
+++ DAO/DAO_Statistics.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+import DTO.DTO_Product;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author phandungtri
+ */
+public class DAO_Statistics {
+    public ArrayList<DTO_Product> getNewestProducts() throws 
+            SQLException, ClassNotFoundException {
+        ArrayList<DTO_Product> products = new ArrayList<DTO_Product>();
+        Connection conn = MySQLConnUtils.getMySQLConnection();
+        String sql = "SELECT * FROM products ORDER BY createdAt LIMIT 10";
+        Statement stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery(sql);
+        
+        while (result.next()) {
+            String id = result.getString(1);
+            String name = result.getString(2);
+            String description = result.getString(3);
+            double price = result.getDouble(4);
+            String publisherID = result.getString(5);
+            String genreID = result.getString(6);
+            String platform = result.getString(7);
+            String releaseDate = result.getString(8);
+            int quantity = result.getInt(9);
+            DTO_Product product = new DTO_Product(id, name, description, publisherID, 
+                    genreID, platform, releaseDate, quantity, price);
+            
+            products.add(product);
+        }
+        conn.close();
+        
+        return products;
+    }
+}
